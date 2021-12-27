c0f905a8d8bc35a50e1fd746b58b02cb3a72c493
Alejandro Velasquez <alejandrovelasquezmolina@gmail.com>
2021-12-27 18:03:21 -0500

    feat: acreando componentes para mostrar las llamadas

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,8 +1,52 @@
+import { useContext } from 'react';
+
+import { Grid, Typography, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+
+import { SocketContext } from '../context/SocketContext';
+
+const useStyles = makeStyles((theme) => ({
+    video: {
+      width: '550px',
+      [theme.breakpoints.down('xs')]: {
+        width: '300px',
+      },
+    },
+    gridContainer: {
+      justifyContent: 'center',
+      [theme.breakpoints.down('xs')]: {
+        flexDirection: 'column',
+      },
+    },
+    paper: {
+      padding: '10px',
+      border: '2px solid black',
+      margin: '10px',
+    },
+  }));
+
+  
 const VideoPlayer = () => {
+    const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext);
+
+    const classes = useStyles();
+
     return (
-        <div>
-            VideoPlayer
-        </div>
+        <Grid container className={classes.gridContainer}>
+            <Paper className={classes.paper}>
+                <Grid item xs={12} md={6}>
+                    <Typography variant="h5" gutterBottom>{ name || 'Name'}</Typography>
+                    <video playsInline muted ref={myVideo} autoPlay className={classes.video} />
+                </Grid>
+            </Paper>
+
+            <Paper className={classes.paper}>
+                <Grid item xs={12} md={6}>
+                    <Typography variant="h5" gutterBottom>{call.name || 'Name'}</Typography>
+                    <video playsInline muted ref={userVideo} autoPlay className={classes.video} />
+                </Grid>
+            </Paper>
+        </Grid>   
     )
 }
 
--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,5 +1,5 @@
 import  {  createContext, useState, useRef, useEffect } from 'react';
-import { connect, io } from 'socket.io-client';
+import { io } from 'socket.io-client';
 import Peer from 'simple-peer';
 
 const SocketContext = createContext();
