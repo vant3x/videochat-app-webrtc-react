4750b8e14a0fa5b1f5ef73768cb0215537bd046e
Alejandro Velasquez <alejandrovelasquezmolina@gmail.com>
2021-12-27 17:08:34 -0500

    feat: terminando la logica del useffect para comenzar la conexión

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -24,12 +24,13 @@ const ContextProvider = ({children}) => {
             socket.on('me', (id) => setMe(id));
 
             socket.on('calluser', ({ from, name: callerName, signal  }) => {    
+                setCall({ isReceivedCall: true , from, name: callerName, signal});
 
             });
     }, []);
 
     const answerCall = () => {
-
+        setCall({ isReceivedCall})
     }
 
     const callUser = () => {
