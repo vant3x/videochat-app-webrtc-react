fff95bfd44d1e4a89db47b10703554b60454c4fc
Alejandro Velasquez <alejandrovelasquezmolina@gmail.com>
2021-12-27 17:19:42 -0500

    feat: implementando el metodo de answerCall para crear conexión

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,5 +1,5 @@
 import  {  createContext, useState, useRef, useEffect } from 'react';
-import { io } from 'socket.io-client';
+import { connect, io } from 'socket.io-client';
 import Peer from 'simple-peer';
 
 const SocketContext = createContext();
@@ -11,8 +11,12 @@ const ContextProvider = ({children}) => {
     const [stream, setStream] = useState(null);
     const [me, setMe] = useState(''); 
     const [call, setCall] = useState(null);
+    const [callAccepted, setCallAccepted] = useState(false); 
+    const [callEnded, setCallEnded ] = useState(false); 
 
     const myVideo = useRef();
+    const userVideo = useRef();
+    const connectionRef = useRef();
 
     useEffect(() => {
         navigator.mediaDevices.getUserMedia({ video: true, audio: true })
@@ -30,7 +34,20 @@ const ContextProvider = ({children}) => {
     }, []);
 
     const answerCall = () => {
-        setCall({ isReceivedCall})
+        setCallAccepted(true);
+
+        const peer = new Peer({ initiator: false,  trickle: false,  stream});
+        peer.on('signal', data => { 
+            socket.emit('answercall', { signal: data, to: call.from });
+        });
+
+        peer.on('stream', (currentStream) => {
+            userVideo.current.srcObject = currentStream;
+        });
+
+        peer.signal(call.signal);
+        
+        connectionRef.current = peer;
     }
 
     const callUser = () => {
